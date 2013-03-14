29175972f344986f4f062bca85cf1ae510707ba2
socketubs <geoffrey@lehee.name>
2013-03-14 01:05:39 +0100

    Add new backend system

---
+++ leselys/backends/_sofart.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from sofart import Database
+
+class Backend(object):
+    def __init__(self, **kwargs):
+        self.path = kwargs['path']
+        self.mode = kwargs['mode']
+        self.db = Database(self.path, self.mode)
+
+    def set_setting(self, key, value):
+        self.db.settings.save({key: value})
+
+    def get_setting(self, key):
+        if not self.db.settings.find_one().get(key, False):
+            return False
+        else:
+            return self.db.settings.find_one()[key]
+
+    def get_settings(self):
+        settings = {}
+        for setting in self.db.settings.find():
+            settings.update(setting)
+        del settings['_id']
+        return settings
+
+    def add_feed(self, content):
+        self.db.feeds.save(content)
+
+    def remove_feed(self, _id):
+        self.db.feeds.remove(_id)
+        for entry in self.db.stories.find({'feed_id': _id}):
+            self.db.stories.remove(entry['_id'])
+
+    def get_feed_by_id(self, _id):
+        return self.db.feeds.find_one({'_id': _id})
+
+    def get_feed_by_title(self, title):
+        return self.db.feeds.find_one({'title': title})
+
+    def get_feeds(self):
+        res = []
+        for feed in self.db.feeds.find():
+            res.append(feed)
+        return res
+
+    def add_story(self, content):
+        return self.db.stories.save(content)
+
+    def remove_story(self, _id):
+        self.db.stories.remove(_id)
+
+    def update_story(self, _id, content):
+        self.db.stories.remove(_id)
+        return self.db.stories.save(content)
+
+    def get_story_by_id(self, _id):
+        return self.db.stories.find_one({'_id': _id})
+
+    def get_story_by_title(self, title):
+        return self.db.stories.find_one({'title': title})
+
+    def get_feed_unread(self, feed_id):
+        res = []
+        for feed in self.db.stories.find({'feed_id': feed_id, 'read': False}):
+            res.append(feed)
+        return res
+
+    def get_stories(self, feed_id):
+        res = []
+        for story in self.db.stories.find({'feed_id': feed_id}):
+            res.append(story)
+        return res
--- leselys/core.py
+++ leselys/core.py
@@ -1,33 +1,28 @@
 # coding: utf-8
 import os
 
-from sofart import Database
 from flask import Flask
-
 from leselys.logger import stream_logger
 
 class Core(object):
-	def __init__(self, path, mode, host, port, debug):
+    def __init__(self, host, port, debug):
+        self.host = host
+        self.port = int(port)
+        self.debug = debug
+        self.backend = None
+        self.backend_settings = None
 
-		self.path = path
-		self.mode = mode
-		self.host = host
-		self.port = int(port)
-		self.debug = debug
+    def run(self):
+        self.backend = self.backend.Backend(**self.backend_settings)
 
-	def run(self):
-		self.db = Database(self.path, mode=self.mode)
-		if not self.db.settings.find_one():
-			self.db.settings.save({'acceptable_elements': []})
+        from leselys.reader import Reader
+        self.reader = Reader()
 
-		from leselys.reader import Reader
-		self.reader = Reader()
+        SECRET_KEY = os.urandom(24)
+        self.app = Flask(__name__)
+        self.app.config.from_object(__name__)
+        self.app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-		SECRET_KEY = os.urandom(24)
-		self.app = Flask(__name__)
-		self.app.config.from_object(__name__)
-		self.app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
-		from leselys import views
-		from leselys import api
-		self.app.run(host=self.host, port=int(self.port), debug=self.debug)
+        from leselys import views
+        from leselys import api
+        self.app.run(host=self.host, port=int(self.port), debug=self.debug)
