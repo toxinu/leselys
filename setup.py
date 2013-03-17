d7d24f50e298be60ae578671a4058589f83048c7
socketubs <geoffrey@lehee.name>
2013-03-17 20:43:21 +0100

    add sofart dep

--- setup.py
+++ setup.py
@@ -43,7 +43,8 @@ setup(
         'pymongo',
         'feedparser',
         'lxml',
-        'itsdangerous'
+        'itsdangerous',
+        'sofart'
     ],
     include_package_data=True,
     classifiers=(
