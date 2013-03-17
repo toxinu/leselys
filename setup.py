0ba6a213467c79ac41768e410cf02e5238c27e04
socketubs <geoffrey@lehee.name>
2013-03-17 20:37:18 +0100

    clean and fix #19

--- setup.py
+++ setup.py
@@ -26,28 +26,6 @@ if sys.argv[-1] == 'publish':
     sys.exit()
 
 setup(
-<<<<<<< HEAD
-	name='leselys',
-	version=get_version(),
-	description="I'm Leselys, your very elegant RSS reader.",
-	long_description=open('README.rst').read(),
-	license=open('LICENSE').read(),
-	author='socketubs',
-	author_email='geoffrey@lehee.name',
-	url='https://github.com/socketubs/leselys',
-	keywords='rss reader greader',
-	packages=['leselys'],
-	scripts=['scripts/leselys'],
-	install_requires=['docopt==0.5.0','flask==0.9','pymongo','feedparser','lxml','itsdangerous'],
-	include_package_data=True,
-	classifiers=(
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 2.6',
-		'Programming Language :: Python :: 2.7')
-=======
     name='leselys',
     version=get_version(),
     description="I'm Leselys, your very elegant RSS reader.",
@@ -58,7 +36,7 @@ setup(
     url='https://github.com/socketubs/leselys',
     keywords='rss reader greader',
     packages=['leselys'],
-    scripts=['scripts/leselys', 'scripts/echo_leselys_conf'],
+    scripts=['scripts/leselys'],
     install_requires=[
         'docopt==0.5.0',
         'flask==0.9',
@@ -75,5 +53,4 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7')
->>>>>>> dafd745fb7bec3bcff88756bd8a4abe4c6f6636f
 )
