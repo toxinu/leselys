9be6f77d3f9ac873f21ad3abd63243f7ce4adbcf
socketubs <geoffrey@lehee.name>
2013-03-17 16:25:49 +0100

    fix login redirect

--- leselys/views.py
+++ leselys/views.py
@@ -72,7 +72,7 @@ def login():
                 try:
                     password_unsigned = signer.unsign(password_md5, max_age=15*24*60*60)
                 except:
-                    return redirect(url_for('login'))
+                    return redirect(render_template('login.html'))
                 if password_unsigner == backend.get_password(username):
                     return redirect(url_for('home'))
     return render_template('login.html')
