56ef5cd34f9b377395a0639e92a1a081301e927c
socketubs <geoffrey@lehee.name>
2013-03-18 22:28:00 +0100

    clean old load backend method

--- leselys/backends/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-def _load_backend(backend_name):
-        backend = __import__("leselys.backends._%s" % backend_name)
-        backend = sys.modules["leselys.backends._%s" % backend_name]
-        if backend is None:
-            raise Exception('Failed to load %s backend' % backend_name)
-        return backend
