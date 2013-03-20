d43d78025a7c8a80b72951df560797cff04ced18
socketubs <geoffrey@lehee.name>
2013-03-20 16:54:25 +0100

    add fade[In|Out] javascript effect

---
+++ leselys/static/js/utils.js
@@ -0,0 +1,34 @@
+function fadeIn(what, duration) {
+  if (what.style.display == "none") {
+    what.style.display = "block";
+  }
+  what.opct = 100;
+  what.ih = window.setInterval(function() {
+    what.opct++;
+    if(what.opct) {
+      what.MozOpacity = what.opct / 100;
+      what.KhtmlOpacity = what.opct / 100;
+      what.filter = "alpha(opacity=" + what.opct + ")";
+      what.opacity = what.opct / 100;
+    }else{
+      window.clearInterval(what.ih);
+      what.style.display = 'block';
+    }
+  }, 10 * duration);
+}
+
+function fadeOut(what, duration) {
+  what.opct = 100;
+  what.ih = window.setInterval(function() {
+    what.opct--;
+    if(what.opct) {
+      what.MozOpacity = what.opct / 100;
+      what.KhtmlOpacity = what.opct / 100;
+      what.filter = "alpha(opacity=" + what.opct + ")";
+      what.opacity = what.opct / 100;
+    }else{
+      window.clearInterval(what.ih);
+      what.style.display = 'none';
+    }
+  }, 10 * duration);
+}
