bbe6db86f9b22f780f73d4b0ab35f994c4ade66c
Geoffrey Lehée <geoffrey@lehee.name>
2013-03-21 01:33:19 +0100

    fix

--- leselys/static/js/utils.js
+++ leselys/static/js/utils.js
@@ -1,61 +1,44 @@
-<<<<<<< HEAD
-function fadeOut( elem, time ) {
-  if (screen.width > 768) {
-    var time = time || 1;
-    elem.style['-webkit-transition'] = "visibility 0s " + time + "s, opacity " + time + "s linear";
-    elem.style['-moz-transition'] = "visibility 0s " + time + "s, opacity " + time + "s linear";
-    elem.style['-o-transition'] = "visibility 0s " + time + "s, opacity " + time + "s linear";
-    elem.style['transition'] = "visibility 0s " + time + "s, opacity " + time + "s linear";
-    elem.style['opacity'] = "0";
-  }
+function hide( elem, callback ) {
+//  elem.style.display = "none";
+  elem.style.visibility = "hidden";
+  if (callback) { return callback() }
+}
+
+function show( elem, callback ) {
+//  elem.style.display = "";
+  elem.style.visibility = "visible";
+  if (callback) { return callback() }
+}
+
+function fadeOut( elem, time, callback) {
+  console.log(screen.width);
+  var time = time || "500ms";
+//  if (screen.width > 768) {
+    elem.style['-webkit-transition'] = "visibility 0s " + time + ", opacity " + time + "s linear";
+    elem.style['-moz-transition'] = "visibility 0s " + time + ", opacity " + time + "s linear";
+    elem.style['-o-transition'] = "visibility 0s " + time + ", opacity " + time + "s linear";
+    elem.style['transition'] = "visibility 0s " + time + ", opacity " + time + "s linear";
+//  }
   elem.style['visibility'] = "hidden";
   elem.style['opacity'] = "0";
+  elem.style.top = "-100000px";
+//  elem.style['display'] = "none";
+  if (callback) { return callback() }
 }
 
-function fadeIn( elem, time ) {
-  elem.style.display = "";
-  elem.style['visibility'] = "visible";
-  if (screen.width > 768) {
-    var time = time || 1;
-    elem.style['-webkit-transition'] = "opacity " + time + "s linear";
-    elem.style['-moz-transition'] = "opacity " + time + "s linear";
-    elem.style['-o-transition'] = "opacity " + time + "s linear";
-    elem.style['transition'] = "opacity " + time + "s linear";
-  }
-  elem.style['opacity'] = "1";
-=======
-function fadeIn(what, duration) {
-  if (what.style.display == "none") {
-    what.style.display = "block";
-  }
-  what.opct = 100;
-  what.ih = window.setInterval(function() {
-    what.opct++;
-    if(what.opct) {
-      what.MozOpacity = what.opct / 100;
-      what.KhtmlOpacity = what.opct / 100;
-      what.filter = "alpha(opacity=" + what.opct + ")";
-      what.opacity = what.opct / 100;
-    }else{
-      window.clearInterval(what.ih);
-      what.style.display = 'block';
-    }
-  }, 10 * duration);
-}
+function fadeIn( elem, time, callback ) {
+    var time = time || "500ms";
+    elem.style.top = "";
+    elem.style.display = "";
+    elem.style['visibility'] = "visible";
+    elem.style['opacity'] = "1";
+    elem.style['-webkit-transition'] = "opacity " + time + " linear";
+    elem.style['-moz-transition'] = "opacity " + time + " linear";
+    elem.style['-o-transition'] = "opacity " + time + " linear";
+    elem.style['transition'] = "opacity " + time + " linear";
 
-function fadeOut(what, duration) {
-  what.opct = 100;
-  what.ih = window.setInterval(function() {
-    what.opct--;
-    if(what.opct) {
-      what.MozOpacity = what.opct / 100;
-      what.KhtmlOpacity = what.opct / 100;
-      what.filter = "alpha(opacity=" + what.opct + ")";
-      what.opacity = what.opct / 100;
-    }else{
-      window.clearInterval(what.ih);
-      what.style.display = 'none';
-    }
-  }, 10 * duration);
->>>>>>> d43d78025a7c8a80b72951df560797cff04ced18
+//    elem.style.display = "";
+    if (callback) { return callback() }
+//  if (screen.width > 768) {
+//  }
 }
