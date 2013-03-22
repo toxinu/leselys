07b2798ded9f4cb3b2ff821a99e8880932c500c3
socketubs <geoffrey@lehee.name>
2013-03-22 10:47:01 +0100

    Add text/xml to guessed content type

--- leselys/feed_finder.py
+++ leselys/feed_finder.py
@@ -6,7 +6,7 @@ class FeedFinder(HTMLParser):
     HTML parser which collects all rss/atom auto discovery links.
     """
     feed_content_types = ['application/rss+xml', 'application/atom+xml',
-            'application/xml']
+            'application/xml', 'text/xml']
 
     def __init__(self):
         HTMLParser.__init__(self)
