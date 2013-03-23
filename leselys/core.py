6f230c59f5368c6bfc1a12ebcdc0ace27c935877
Geoffrey Lehée <geoffrey@lehee.name>
2013-03-23 20:08:05 +0100

    start background refresher with celery

--- leselys/core.py
+++ leselys/core.py
@@ -1,34 +1,80 @@
 # coding: utf-8
 import os
+import sys
+import ConfigParser
 
 from itsdangerous import TimestampSigner
 from flask import Flask
 from werkzeug.contrib.cache import SimpleCache
 
+from leselys.backends.storage import _load_storage
+from leselys.backends.session import _load_session
 
 class Core(object):
-    def __init__(self, host, port, debug):
-        self.host = host
-        self.port = int(port)
-        self.debug = debug
+    def __init__(self):
+        self.host = "127.0.0.1"
+        self.port = 5000
+        self.debug = False
         self.storage = None
-        self.storage_settings = None
+        self.storage_settings = {}
         self.session = None
-        self.session_settings = None
+        self.session_settings = {}
 
         self.app = Flask(__name__)
         self.app.config['SECRET_KEY'] = os.urandom(24)
         self.signer = TimestampSigner(self.app.config['SECRET_KEY'])
         self.cache = SimpleCache()
 
-    def load_storage(self, storage_klass, storage_settings):
-        self.storage_settings = storage_settings
-        self.storage = storage_klass(**self.storage_settings)
+    def load_config(self, config_path, args={}):
+        if not os.path.exists(config_path):
+            print('Error: "%s" file not exists.' % config_path)
+            sys.exit(1)
+        self.args = args
+        self.config_path = config_path
+        self.config = ConfigParser.ConfigParser()
+        self.config.read(self.config_path)
+
+        if self.config.has_section('webserver') and self.config.get('webserver', 'host'):
+            self.host = self.config.get('webserver', 'host')
+        if self.config.has_section('webserver') and self.config.get('webserver', 'port'):
+            self.port = self.config.get('webserver', 'port')
+        if self.config.has_section('webserver') and self.config.get('webserver', 'debug'):
+            if self.config.get('webserver', 'debug') in ['True', 'true']:
+                self.debug = True
+            else:
+                self.debug = False
+        self.host = self.host or self.args.get('--host')
+        self.port = self.port or self.args.get('--port')
+        self.debug = self.debug or self.args.get('--debug')
+
+        if not self.config.has_section('storage'):
+            print('Missing storage section in configuration file')
+            sys.exit(1)
+        if not self.config.get('storage', 'type'):
+            print('Missing type setting in storage section in configuration file')
+            sys.exit(1)
+        if not self.config.has_section('session'):
+            self.config.add_section('session')
+            self.config.set('session', 'type', 'memory')
+
+    def load_storage(self):
+        for item in self.config.items('storage'):
+            self.storage_settings[item[0]] = item[1]
+        del self.storage_settings['type']
+
+        self.storage_module = _load_storage(self.config.get('storage', 'type'))
+        self.storage =  self.storage_module(**self.storage_settings)
 
     def load_session(self):
         if self.session == "memory":
             return
-        self.session = self.session.Session(**self.session_settings)
+        
+        for item in self.config.items('session'):
+            self.session_settings[item[0]] = item[1]
+        del self.session_settings['type']
+
+        self.session_module = _load_session(self.config.get('session', 'type'))
+        self.session = self.session_module.Session(**self.session_settings)
         self.app.session_interface = self.session
 
     def load_wsgi(self):
---
+++ leselys/worker.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import sys
+from celery import Celery
+from leselys import core
+from datetime import timedelta
+
+
+def run(config_path, args):
+	celery = Celery('tasks', broker='mongodb://localhost:27017/leselys')
+
+	args.append('-B')
+	core.load_config(config_path)
+	core.load_storage()
+	core.load_session()
+
+	@celery.task
+	def refresh_all():
+		from leselys.reader import Refresher
+		feeds = core.storage.get_feeds()
+		for feed in feeds:
+			print(feed['title'])
+			refresher = Refresher(feed)
+			refresher.start()
+
+	celery.conf.CELERYBEAT_SCHEDULE = {
+	'refresh-every-30-seconds': {
+    	'task': 'leselys.worker.refresh_all',
+    	'schedule': timedelta(seconds=30)
+    	},
+	}
+	celery.conf.INSTALLED_APPS = ('tasks.refresh_all',)
+
+	print('Args: %s' % ' '.join(args))
+	celery.start(args)
