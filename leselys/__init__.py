b0d6f6bf68e09c8e34af495e30d0a4052eed3efb
Geoffrey Lehée <geoffrey@lehee.name>
2013-04-18 17:27:42 +0200

    0.1.10

--- leselys/__init__.py
+++ leselys/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 __title__ = 'leselys'
-__version__ = '0.1.9'
+__version__ = '0.1.10'
 __author__ = 'Geoffrey Lehée'
 __license__ = ''
 __copyright__ = 'Copyright 2013 Geoffrey Lehée'
