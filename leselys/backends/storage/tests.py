dabc945e965064dabdb97555db2812f4c1b8090e
David Buxton <david@gasmark6.com>
2013-07-22 21:55:50 +0100

    Failing tests for any abstract Storage backend.

--- leselys/backends/storage/tests.py
+++ leselys/backends/storage/tests.py
@@ -11,5 +11,29 @@ class StorageTestCase(unittest.TestCase):
     def test_api(self):
         # Basic API for any Storage backend.
         obj = Storage()
+        obj.add_feed
+        obj.add_story
+        obj.all_stories
+        obj.get_feed_by_id
+        obj.get_feed_by_title
+        obj.get_feed_count
+        obj.get_feed_setting
+        obj.get_feed_unread
+        obj.get_feed_unread_count
+        obj.get_feeds
+        obj.get_password
+        obj.get_setting
+        obj.get_settings
+        obj.get_stories
+        obj.get_story_by_guid
+        obj.get_story_by_id
+        obj.get_story_by_title
         obj.is_valid_password
+        obj.remove_feed
+        obj.remove_story
+        obj.set_feed_setting
+        obj.set_password
+        obj.set_setting
+        obj.update_feed
         obj.update_password
+        obj.update_story
