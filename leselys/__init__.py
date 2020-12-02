9e9a31991c70e157f85bbb61aaafa8d9cd564047
toxinu <toxinu@gmail.com>
2020-12-03 00:45:34 +0900

    Update

--- leselys/__init__.py
+++ leselys/__init__.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 
 __title__ = 'leselys'
-__version__ = '0.2.6'
-__author__ = 'Geoffrey Lehée'
+__version__ = '0.2.7'
+__author__ = 'toxinu'
 __license__ = ''
-__copyright__ = 'Copyright 2013 Geoffrey Lehée'
+__copyright__ = 'Copyright 2013 toxinu'
 
 from leselys.core import Core
 
